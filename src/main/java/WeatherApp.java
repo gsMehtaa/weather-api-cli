494e2e377864d4b18b2a84142ff2418bba8c24d5
gSingh05 <mehtashawn10@gmail.com>
2026-03-07 01:11:01 -0500

    Initial Maven weather API project. Weather App: -User inputs a specific city, the program replies with the temperature in that city plus a few other metrics. -User will enter a city, which would send an API request to the weather server, and the server will return the weather data in JSON. -We will then parse the JSON -Convert into variables (for better readability) -The program will print the metrics.

---
+++ src/main/java/WeatherApp.java
@@ -0,0 +1,30 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import org.json.JSONObject;
+
+/*
+Weather App:
+-User inputs a specific city, the program replies with the temperature in that city plus a few other metrics.
+-User will enter a city, which would send an API request to the weather server, and the server will return the
+weather data in JSON.
+-We will then parse the JSON
+-Convert into variables (for better readability)
+-The program will print the metrics.
+ */
+
+
+
+
+public class WeatherApp {
+
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("Weather App Starting...");
+    }
+}
